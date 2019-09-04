843d9fa7f04d356bc75d6f9d40c0cf13a11074ce
Kay Ohtie <kay@coyotesin.space>
2019-09-03 20:27:23 -0500

    Updated to pull directly from google

--- build_zip.sh
+++ build_zip.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 mkdir -p build
 mkdir -p bin
-wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.3-linux-x86-64.tar.gz -O - | tar xz - libwebp-1.0.3-linux-x86-64/bin/dwebp > bin/dwebp
+wget -c https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.3-linux-x86-64.tar.gz -O - | tar xOz libwebp-1.0.3-linux-x86-64/bin/dwebp > bin/dwebp
 zip -r build/package.zip bin/* lambda_function.py
